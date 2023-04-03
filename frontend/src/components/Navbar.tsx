9e96a40300ef1762fe619b722e1e3efdeef16d28
Vegard Lokreim <vegardlokreim9@gmail.com>
2023-04-03 17:23:34 +0200

    Setting up login routes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,27 +4,15 @@ import Link from "next/link";
 import React from "react";
 import { useSelector } from "react-redux";
 
-type Props = {
-  totalQuantity: number;
-};
-
 export default function Navbar() {
-  const totalQuantity = useSelector((state: RootState) => state.cart.totalQuantity);
-
-  console.log();
   return (
     <nav className="flex bg-black text-white justify-between items-center p-8 text-xl">
-      <div>Logo</div>
+      <div>
+        <Link href="/">Logo</Link>
+      </div>
       <ul className="flex gap-4">
         <li>
           <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/shop">Shop</Link>
-        </li>
-
-        <li>
-          <Link href="/cart">Cart ({totalQuantity})</Link>
         </li>
       </ul>
     </nav>
--- server/index.js
+++ server/index.js
@@ -1,13 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-// import woocommerceRouter from "./routes/woocommerce.js"
-// import klarnaRouter from "./routes/klarna.js"
-
-import ordersRouter from "./routes/orders.js"
-import productsRouter from "./routes/products.js"
-
+import jwt from "jsonwebtoken"
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -16,4 +10,35 @@ app.use(express.json());
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+
+function authenticateJWT(req, res, next) {
+    const token = req.headers.authorization.split(' ')[1];
+
+    if (token) {
+        jwt.verify(token, 'sO_F>%0{*!-2@d/[)nx2t_]G_P-?KROn:Ibc=x9wvq$>&03QO+mQ/[._ck82zz', (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+app.get('/products', authenticateJWT, async (req, res) => {
+    try {
+        const response = await axios.get('https://your-wordpress-site.com/wp-json/wc/v3/products', {
+            headers: {
+                Authorization: `Bearer ${req.user.token}`
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send('Error fetching products');
+    }
+});
 
