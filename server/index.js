5d6c45cd0c1654cac4315058a8668373a1403cb9
Vegard Lokreim <vegardlokreim9@gmail.com>
2023-04-05 22:06:14 +0200

    Added cors args to fix login route

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ import authRouter from "./routes/auth.js"
 dotenv.config();
 const app = express();
 app.use(cors({
-
+    origin: 'http://localhost:3000',
+    credentials: true,
 }));
 app.use(express.json());
 
